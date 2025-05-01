fb486a6bc799deb3835cf5ee11c5208419d79e33
Adrian Hitz <hitzadr1@students.zhaw.ch>
2025-05-01 21:04:09 +0200

    FizzBuzz Code Ergänzt + Test

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
@@ -3,14 +3,19 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 public class FizzBuzzConverter {
 
     public String convert(int i) {
-        if (i % 3 == 0 && i % 7 == 0) {
-            return "FizzBuzz";
-        } else if (i % 3 == 0) {
-            return "Fizz";
-        } else if (i % 7 == 0) {
-            return "Buzz";
-        }
-        return String.valueOf(i);
-    }
+        StringBuilder result = new StringBuilder();
 
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+        if (i % 7 == 0) {
+            result.append("DevOps");
+        }
+
+        return result.length() > 0 ? result.toString() : String.valueOf(i);
+    }
 }
+
--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
@@ -5,53 +5,55 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzzConverterTest {
 
+    FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
     @Test
     public void fizzBuzzConverter1() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
         Assertions.assertEquals("1", fizzBuzz.convert(1));
-
     }
 
     @Test
     public void fizzBuzzConvertor2() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
         Assertions.assertEquals("2", fizzBuzz.convert(2));
-
     }
 
     @Test
     public void fizzBuzzConvertor3() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
         Assertions.assertEquals("Fizz", fizzBuzz.convert(3));
     }
-    
+
     @Test
     public void fizzBuzzConvertorMultiplesOfThree() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
         Assertions.assertEquals("Fizz", fizzBuzz.convert(6));
     }
 
     @Test
-    void fizzBuzzConvertorMultiplesOfSeven() {
-
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
-        Assertions.assertEquals("Buzz", fizzBuzz.convert(7));
+    public void fizzBuzzConvertorMultiplesOfFive() {
+        Assertions.assertEquals("Buzz", fizzBuzz.convert(5));
     }
 
     @Test
-    void fizzBuzzConvertorMultiplesOfThreeAndSeven() {
-        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-        Assertions.assertNotEquals("FizzBuzz", fizzBuzz.convert(14));
-        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(21));
-        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(42));
-        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(63));
+    public void fizzBuzzConvertorMultiplesOfSeven() {
+        Assertions.assertEquals("DevOps", fizzBuzz.convert(7));
     }
 
-}
+    @Test
+    public void fizzBuzzConvertorThreeAndFive() {
+        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(15));
+    }
+
+    @Test
+    public void fizzBuzzConvertorThreeAndSeven() {
+        Assertions.assertEquals("FizzDevOps", fizzBuzz.convert(21));
+    }
+
+    @Test
+    public void fizzBuzzConvertorFiveAndSeven() {
+        Assertions.assertEquals("BuzzDevOps", fizzBuzz.convert(35));
+    }
+
+    @Test
+    public void fizzBuzzConvertorThreeFiveSeven() {
+        Assertions.assertEquals("FizzBuzzDevOps", fizzBuzz.convert(105));
+    }
+} 
