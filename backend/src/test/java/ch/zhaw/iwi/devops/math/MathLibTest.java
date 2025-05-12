1d416ea5cee6e4f08e074c6f80f81c9b5767da5a
Adrian Hitz <hitzadr1@students.zhaw.ch>
2025-05-12 15:54:37 +0200

    fixes

--- backend/src/test/java/ch/zhaw/iwi/devops/math/MathLibTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/math/MathLibTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static
 org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+
 public class MathLibTest {
 @Test
 public void testEven1() {
